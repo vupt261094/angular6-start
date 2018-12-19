c10380530c5484dba5ebf8077cefb22460159973
Vu Pham <vupt261094@gmail.com>
2018-12-19 21:51:42 +0800

    .

--- src/modules/user-profile/user-profile.route.ts
+++ src/modules/user-profile/user-profile.route.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: '',
     pathMatch: 'prefix',
     component: AuthorizeLayoutComponent,
-    canActivate: [IsAuthorizedGuard],
+    // canActivate: [IsAuthorizedGuard],
     children: [
       {
         path: '',
